cc8cdcee6560876bef4d96b635110a07aef7a2bb
Andrey Kostenko <akoste01@gmail.com>
2023-02-18 22:40:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ st.header('Breakfast Menu')
 st.text('Omega 3 & Blueberry Oatmeal')
 st.text('Kale, Spinach & Rocket Smoothie')
 st.text('Hard-Boiled Free-Range Egg')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
